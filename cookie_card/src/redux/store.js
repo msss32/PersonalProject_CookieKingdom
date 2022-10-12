92a57e7e4036daa261b337260cba2576483126c4
msss32 <msss14@naver.com>
2022-10-12 17:22:07 +0900

    first

---
+++ cookie_card/src/redux/store.js
@@ -0,0 +1,13 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { loginReducer } from "./reducer";
+
+const rootReducer = combineReducers({
+  login: loginReducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [],
+});
+
+export default store;
